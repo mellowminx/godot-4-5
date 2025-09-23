473c7e4e306123d8911f949cc57caa55893f6a23
mellowminx <126353737+mellowminx@users.noreply.github.com>
2025-09-23 23:50:06 +0800

    Update mouse_passthrough_polygon_generated_from_texture.gd

--- mouse_passthrough_polygon_generated_from_texture.gd
+++ mouse_passthrough_polygon_generated_from_texture.gd
@@ -31,7 +31,7 @@ func _ready() -> void:
 	var polygon_from_bitmap = bitmap.opaque_to_polygons(Rect2(Vector2(), image.get_size()))
 	generated_polygon = polygon_from_bitmap[0]
   # The Polygon2D node to which this script is attached will have its polygon replaced with this dynamically generated one
-	polygon = dynamic_polygon
+	polygon = generated_polygon
 
 func _physics_process(delta: float) -> void:
 	get_window().mouse_passthrough = false
@@ -44,3 +44,51 @@ func getPolygon() -> PackedVector2Array:
 		a.append((point * window_scale) + ((self.global_position - (my_texture.get_size()/2)) * window_scale))
 	var pv2a := PackedVector2Array(a)
 	return pv2a
+
+# optional - draw polygon
+#func _on_draw() -> void:
+	#draw_polyline(getPolygon(), Color.AQUA)
+
+
+
+
+
+
+
+
+extends Polygon2D
+
+@onready var my_texture = $"../fishbowl-button".texture_normal
+
+var initial_window_width: float
+
+var generated_polygon
+
+func _ready() -> void:
+	initial_window_width = ProjectSettings.get_setting("display/window/size/viewport_width")
+
+	# Get the image from the texture normal
+	var image = my_texture.get_image()
+	# Create the BitMap
+	var bitmap = BitMap.new()
+	# Fill it from the image alpha
+	bitmap.create_from_image_alpha(image)
+	# Create polygon
+	var polygon_from_bitmap = bitmap.opaque_to_polygons(Rect2(Vector2(), image.get_size()))
+	generated_polygon = polygon_from_bitmap[0]
+	polygon = generated_polygon
+
+func _physics_process(delta: float) -> void:
+	get_window().mouse_passthrough = false
+	get_window().mouse_passthrough_polygon = getPolygon()
+
+func getPolygon() -> PackedVector2Array:
+	var window_scale = get_window().size.x / initial_window_width
+	var a := []
+	for point in polygon:
+		a.append((point * window_scale) + ((self.global_position - (my_texture.get_size()/2)) * window_scale))
+	var pv2a := PackedVector2Array(a)
+	return pv2a
+
+#func _on_draw() -> void:
+	#draw_polyline(getPolygon(), Color.AQUA)
