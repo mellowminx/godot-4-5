713459d29815b88ba4c95295063eeb89a1bfb464
mellowminx <126353737+mellowminx@users.noreply.github.com>
2026-08-15 12:56:03 +0800

    Update cheatsheet.gd

--- cheatsheet.gd
+++ cheatsheet.gd
@@ -12,3 +12,6 @@ instance.is_queued_for_deletion()
 @export var array_of_images: Array[CompressedTexture2D]
 
 @export_enum("apple", "orange", "banana") var fruit := "apple"
+
+enum State { IDLE, WALK, RUN, JUMP }
+var current_state: State = State.IDLE
